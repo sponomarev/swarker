4a598c16c9c9d413e2ccae32e35776a52111c292
Sergey Ponomarev <me@sergey-ponomarev.ru>
2015-09-06 17:36:06 +0300

    Add swarker cli binary

---
+++ lib/swarker/cli.rb
@@ -0,0 +1,41 @@
+require 'thor'
+
+module Swarker
+  class Cli < Thor
+    include Thor::Actions
+
+    desc 'convert', 'Converts lurker schema to swaggers one'
+    option :input, aliases: '-i', desc: 'Input path', default: 'lurker'
+    option :output, aliases: '-o', desc: 'Output path', default: 'public'
+    option :force, aliases: '-f', type: :boolean, desc: 'Rewrite output without confirmation'
+
+    def convert
+      say_status :input, input
+      say_status :output, output
+
+      services.each do |service|
+        schema = Swarker::Serializers::ServiceSerializer.new(service).schema
+
+        create_file(File.join(output, "#{service.host}.json"), JSON.pretty_generate(schema), force: force?)
+      end
+    end
+
+    private
+
+    def services
+      Swarker::Readers::ServiceReader.new(input).services
+    end
+
+    def input
+      File.expand_path(options[:input])
+    end
+
+    def output
+      File.expand_path(options[:output])
+    end
+
+    def force?
+      options[:force]
+    end
+  end
+end
--- swarker.gemspec
+++ swarker.gemspec
@@ -22,12 +22,13 @@ Gem::Specification.new do |spec|
     fail 'RubyGems 2.0 or newer is required to protect against public gem pushes.'
   end
 
-  spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features|public|lurker)/}) }
   spec.bindir        = 'exe'
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
 
-  spec.add_dependency('activesupport', '~> 4.2')
+  spec.add_dependency 'activesupport', '~> 4.2'
+  spec.add_dependency 'thor'
 
   spec.add_development_dependency 'bundler', '~> 1.10'
   spec.add_development_dependency 'rake', '~> 10.0'
