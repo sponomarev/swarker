4c2c393536f96baa2a145cf312399a7359e57971
Sergey Ponomarev <me@sergey-ponomarev.ru>
2015-09-06 18:36:51 +0300

    Refactor Path to store schema in HashWithIndifferentAccess

--- lib/swarker/path.rb
+++ lib/swarker/path.rb
@@ -3,46 +3,46 @@ module Swarker
   class Path
     attr_reader :name, :schema
 
-    DEFAULT_SCHEME = {
-      'produces' => ['application/json']
+    DEFAULT_SCHEMA = {
+      produces: ['application/json'.freeze]
     }
 
     def initialize(name, original_schema, preparsed = false)
       @name            = name
-      @original_schema = original_schema
+      @original_schema = HashWithIndifferentAccess.new(original_schema)
       @preparsed       = preparsed
 
       if preparsed
         @schema = @original_schema
       else
-        @schema = {}
-        parse_scheme
+        @schema = HashWithIndifferentAccess.new
+        parse_schema
       end
     end
 
     def verb
-      @preparsed ? @schema.keys.first : original_schema['extensions']['method'].downcase
+      @preparsed ? @schema.keys.first : original_schema[:extensions][:method].downcase
     end
 
     private
 
     attr_reader :original_schema
 
-    def parse_scheme
-      @schema[verb] = DEFAULT_SCHEME.merge(computed_scheme)
+    def parse_schema
+      @schema[verb] = DEFAULT_SCHEMA.merge(computed_schema)
     end
 
-    def computed_scheme
+    def computed_schema
       {
-        'description' => description,
-        'tags'        => tags,
-        'parameters'  => parameters,
-        'responses'   => responses
+        description: description,
+        tags:        tags,
+        parameters:  parameters,
+        responses:   responses
       }
     end
 
     def description
-      original_schema['description']
+      original_schema[:description]
     end
 
     def tags
@@ -51,13 +51,13 @@ module Swarker
 
     # TODO: add logic for name parameters
     def parameters
-      @original_schema['requestParameters']['properties'].collect do |parameter, options|
+      @original_schema[:requestParameters][:properties].collect do |parameter, options|
         {
-          'name'        => parameter,
-          'description' => options['description'],
-          'type'        => options['type'],
-          'default'     => options['example'],
-          'in'          => determine_in(parameter)
+          name:        parameter,
+          description: options[:description],
+          type:        options[:type],
+          default:     options[:example],
+          in:          determine_in(parameter)
         }
       end
     end
@@ -65,26 +65,27 @@ module Swarker
     def responses
       {
         response_code => {
-          'description' => '',
-          'schema'      => response_schema
+          description: '',
+          schema:      response_schema
         }
       }
     end
 
     def response_code
-      @original_schema['responseCodes'].first['status'].to_s
+      # Lurker schema has only one response code per file
+      @original_schema[:responseCodes].first[:status].to_s
     end
 
     def response_schema
-      response_schema = @original_schema['responseParameters'].dup
-      response_schema['items'].reject! { |k| k == 'required' } if response_schema['items']
+      response_schema = @original_schema[:responseParameters].dup
+      response_schema[:items].reject! { |k| k.to_sym == :required } if response_schema[:items]
 
       response_schema
     end
 
     # TODO: add logic for formData
     def determine_in(_parameter)
-      'query'
+      'query'.freeze
     end
   end
 end
